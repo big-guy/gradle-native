81f860d50a0abe7e2d96d7fb85e90d1dd2f23da4
Daniel Lacasse <daniel@nokee.dev>
2020-03-20 00:31:11 -0400

    Dfd

--- subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/platform/jni/JniLibraryTargetMachinesFunctionalTest.groovy
+++ subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/platform/jni/JniLibraryTargetMachinesFunctionalTest.groovy
@@ -42,7 +42,7 @@ class JniLibraryTargetMachinesFunctionalTest extends AbstractTargetMachinesFunct
 
 	@Override
 	protected String getTaskNameToAssembleDevelopmentBinaryWithArchitecture(String architecture) {
-		return "main${architecture.capitalize()}SharedLibraryMainCpp"
+		return "main${architecture.capitalize()}SharedLibrary"
 	}
 
 	@Override
